4e5d0f742d1b3551d71b240d633207984a7c23f5
RoneiCarlos <roneicarloskunkel1@protonmail.com>
2021-07-26 03:43:57 -0300

    Correção nas representações de um elemento e de uma lista, criação dos método de inserir e de procurar valor de acordo com uma posição e de limpar uma lista

--- Elemento.py
+++ Elemento.py
@@ -1,27 +1,28 @@
 class Elemento:
     def __init__(self, dado, elemento) -> None:
         '''Construtor de um elemento
-        
+
         Recebe um valor para o elemento e outro elemento para qual ele aponta'''
         self.__dado = dado
         self.__proximo = elemento
 
     def __repr__(self) -> str:
         '''Representação de um elemento'''
-        string = ''
-        if (self.getProximoElemento()):
-            string += str(self.getElemento()) + ' ➞ ' + str(self.getProximoElemento())
-            return string
-        else:
-            return string + str(self.getElemento())
+        return str(self.getDado())
+        # string = ''
+        # if (self.getProximoElemento()):
+        #     string += str(self.getDado()) + ' ➞ ' + str(self.getProximoElemento())
+        #     return string
+        # else:
+        #     return string + str(self.getDado())
 
-    def getElemento(self) -> object:
+    def getDado(self) -> object:
         '''Retorna o dado de um elemento'''
         return self.__dado
 
     def getProximoElemento(self) -> any:
         '''Retorna o elemento seguinte ao atual 
-        
+
         ou False caso não houver'''
         if (self.__proximo == None):
             return False
@@ -30,4 +31,3 @@ class Elemento:
     def setProximo(self, elemento) -> None:
         '''Define qual o próximo elemento do atual'''
         self.__proximo = elemento
-
--- ListaEncadeada.py
+++ ListaEncadeada.py
@@ -2,11 +2,11 @@ from Elemento import Elemento
 
 #-------------------------------------------------------#
 #   Checklist:                                          #
-#       Inserir na posição | Valor - Posição            #
+#  ok   Inserir na posição | Valor - Posição            #
 #       Remover da posição                              #
-#       Procurar o valor de acordo com a posição        #
+#  ok   Procurar o valor de acordo com a posição        #
 #       Procurar a posição de acordo com o valor        #
-#       Destruir a lista                                #
+#  ok   Destruir a lista                                #
 #-------------------------------------------------------#
 
 class Lista:
@@ -14,58 +14,94 @@ class Lista:
         '''Construtor de lista'''
         self.__primeiro = None
         self.__quantidade = 0
-    
+
     def __repr__(self) -> str:
         '''Representação de lista'''
         string = '['
         if (self.getPrimeiro()):
-            string += str(self.getPrimeiro()) + ']'
-            return string.replace(' ➞ ', ', ')
+            return self.recursaoRepresentacao(self.getQuantidadeElementos(), self.getPrimeiro(), string) + ']'
         else:
             return string + ']'
 
-    def getPrimeiro(self) -> any:
-        '''Retorna o elemento que encabeça a lista 
+    def recursaoRepresentacao(self, tamanho, elemento, string):
+        if (tamanho == 1):
+            string += elemento.getDado()
+            return string
         
-        ou False caso não houver'''
+        tamanho -= 1
+        string += elemento.getDado() + ', '
+        proximoElemento = elemento.getProximoElemento()
+        return self.recursaoRepresentacao(tamanho, proximoElemento, string)
+
+    def clearLista(self) -> None:
+        '''Limpa a lista'''
+        self.setPrimeiro(None)
+        self.setQuantidadeElementos(0)
+
+    def getPrimeiro(self) -> any:
+        '''Retorna o primeiro elemento da lista 
+
+        ou False caso a lista esteja vazia'''
         if (self.__primeiro == None):
             return False
         return self.__primeiro
 
+    def setPrimeiro(self, elemento) -> None:
+        '''Insere um elemento na primeira posição'''
+        self.__primeiro = elemento
+
+    def incrementaQuantidadeElementos(self) -> None:
+        '''Incrementa a quantidade de elementos da lista'''
+        self.__quantidade += 1
+
     def insertElementoNaPosicao(self, dado, posicao) -> bool:
-        '''Insere um elemento na posição passada'''
+        '''Insere um elemento na posição desejada
+
+        Retorna True em caso de sucesso
+        ou False em caso de erro'''
+        if (posicao < 1 or posicao > self.getQuantidadeElementos()+1):
+            return False
+
         elemento = Elemento(dado, None)
 
         if (posicao == 1):
             elemento.setProximo(self.getPrimeiro())
             self.setPrimeiro(elemento)
-            return True
-        if (posicao >= 2):
-            return False
-        
-        if (posicao == self.getQuantidadeElementos(self.getPrimeiro())):
-            pass
+
+        if (posicao > 1 and posicao <= self.getQuantidadeElementos()+1):
+            elementoAnterior = self.getElementoNaPosicao(posicao-1)
+            proximoElemento = self.getElementoNaPosicao(posicao-1).getProximoElemento()
+            elemento.setProximo(proximoElemento)
+            elementoAnterior.setProximo(elemento)
+
+        self.incrementaQuantidadeElementos()
+        return True
 
     def getQuantidadeElementos(self) -> int:
-
-        self.recursao(self.getPrimeiro())
-
+        '''Retorna a quantidade de elementos da lista'''
         return self.__quantidade
 
-    def recursao(self, elemento) -> int:
-        '''Retorna a quantidade de elementos da lista'''
+    def setQuantidadeElementos(self, quantidade) -> None:
+        '''Insere a quantidade de elementos na lista'''
+        self.__quantidade = quantidade
 
-        if (not elemento.getProximoElemento()):
-            self.__quantidade += 1
-            return self.__quantidade
+    def recursaoGetElementoNaPosicao(self, posicao, elemento) -> str:
+        '''Recursividade usada para encontrar o elemento na posição desejada'''
+        if (posicao == 1):
+            return elemento.getProximoElemento()
 
         proximoElemento = elemento.getProximoElemento()
-        self.__quantidade += 1
-        return self.recursao(proximoElemento)
+        posicao -= 1
+        return self.recursaoGetElementoNaPosicao(posicao, proximoElemento)
 
+    def getElementoNaPosicao(self, posicao) -> any:
+        '''Retorna o elemento na posição desejada
 
+        ou False caso não haja a posição na lista'''
+        if (posicao < 1 or posicao > self.getQuantidadeElementos()):
+            return False
 
-    #DEDBUGS
-    def setPrimeiro(self, elemento) -> None:
-        '''Insere um elemento na primeira posição'''
-        self.__primeiro = elemento
+        if (posicao == 1):
+            return self.getPrimeiro()
+
+        return self.recursaoGetElementoNaPosicao(posicao-1, self.getPrimeiro())
